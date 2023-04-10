6f1de998052cbcb3bb0b496e09566f7d35750f29
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2023-04-10 16:02:03 -0500

    feat: :rocket: Inicio del taller templates
    Configuracion de las bases con los primeros modulos

---
+++ app.js
@@ -0,0 +1,4 @@
+import {createapp} from 'https://unpkg.com/browse/vue@3.2.47/dist/vue.esm-browser.prod.js'
+import myModule from './components/my-module';
+
+createapp(myModule).mount(`#app`)
---
+++ components/my-module.js
@@ -0,0 +1,6 @@
+export default{
+    name:my-modulo,
+    template: /*html */`
+    
+    `,
+}
